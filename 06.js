38c21f94d6cf2da5a79d0082ecf5c78922fbe3e1
Demedik <demedik1988@gmail.com>
2016-02-01 00:14:56 +0200

    Update 06.js

--- 06.js
+++ 06.js
@@ -105,3 +105,18 @@ function match (array1,array2) {
 	}	
 	return a
 }
+// 6 Вывод значений, которые повторяются
+function matchResult (array1,array2) { 
+	var a = [] 
+	for (i = 0; i < array1.length; i++) { 
+		for (j = 0; j < array2.length; j++) { 
+			if (array1[i] == array2[j]) { 
+				if (a.indexOf(array1[i]) ==  -1) 
+					{a.push(array1[i]);
+				}
+				break;
+			} 
+		} 
+	} 
+return a 
+}
